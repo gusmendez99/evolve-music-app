3d3ab3be3b1ed6cbf13550a842d637ebcaa399c8
Gus Mendez <gus.mendez.99@gmail.com>
2020-03-19 13:54:07 -0600

    Error on API login fixed

--- client/src/components/UserListItem/index.js
+++ client/src/components/UserListItem/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import CustomLink from '../CustomLink';
 import { withRouter, Redirect } from "react-router";
 

8af66d2dab52ebfaebee8d1eb2452393bd0dee1d
luis2 <luis212urbina@gmail.com>
2020-03-24 23:00:07 -0600

    minor changes made in report.js

--- api/routes/report.js
+++ api/routes/report.js
@@ -7,14 +7,8 @@ const GET_PLAYLISTS_BY_DURATION = "SELECT pl.Name, cast(SUM((t.Milliseconds)/600
 const GET_LONGEST_SONGS = "SELECT t.TrackId, t.Name as Track, a.Name as Artist, al.Title as Album, cast((t.Milliseconds/60000.0) as decimal(10,2)) as Minutes from Track t INNER JOIN album al on al.AlbumId = t.AlbumId INNER JOIN artist a on al.ArtistId = a.ArtistId ORDER BY t.Milliseconds DESC LIMIT 5";
 const GET_USERS_WITH_MORE_TRACKS_REGISTER = "SELECT u.UserName, count(u.UserId) as TracksRegister FROM TrackHistory tah INNER JOIN AppUser u ON u.UserId = tah.UserId GROUP BY u.UserId ORDER BY count(u.UserId) DESC LIMIT 5";
 const GET_GENRES_DURATION_AVG = "SELECT g.GenreId, g.Name as Genre, cast((avg(t.Milliseconds)/60000.0) as decimal(10,2)) as avg_minutes from track t INNER JOIN genre g on t.GenreId = g.GenreId GROUP BY g.GenreId, g.Name ORDER BY avg(t.Milliseconds) DESC";
-<<<<<<< HEAD
 const GET_COUNT_ARTIST_BY_PLAYLIST = "SELECT ArtistPlayList.Name, COUNT(ArtistPlayList.Name) as artists FROM (SELECT pl.Name as Name, COUNT(t.AlbumId) as ArtistCount FROM PlaylistTrack plt INNER JOIN Playlist pl ON plt.PlaylistId = pl.PlaylistId INNER JOIN Track t ON plt.TrackId = t.TrackId GROUP BY (pl.Name, t.AlbumId)) ArtistPlayList GROUP BY ArtistPlayList.name ORDER BY COUNT(ArtistPlayList.Name) DESC";
 const GET_ARTIST_WITH_MORE_GENRES = "SELECT a.Name, COUNT(a.Name) as genres FROM (SELECT a.ArtistId, t.GenreId FROM Artist a INNER JOIN Album al ON a.ArtistId = al.ArtistId INNER JOIN Track t ON al.AlbumId = t.AlbumId GROUP BY(a.ArtistId, t.GenreId)) ArtistGenre INNER JOIN Artist a ON ArtistGenre.ArtistId = a.ArtistId INNER JOIN Genre g ON ArtistGenre.GenreId = g.GenreId GROUP BY (a.Name) ORDER BY COUNT(a.Name) DESC LIMIT 5";
-=======
-const GET_COUNT_ARTIST_BY_PLAYLIST = "SELECT ArtistPlayList.Name, COUNT(ArtistPlayList.Name) FROM (SELECT pl.Name as Name, COUNT(t.AlbumId) as ArtistCount FROM PlaylistTrack plt INNER JOIN Playlist pl ON plt.PlaylistId = pl.PlaylistId INNER JOIN Track t ON plt.TrackId = t.TrackId GROUP BY (pl.Name, t.AlbumId)) ArtistPlayList GROUP BY ArtistPlayList.name ORDER BY COUNT(ArtistPlayList.Name) DESC"
-const GET_ARTIST_WITH_MORE_GENRES = "SELECT a.Name, COUNT(a.Name) FROM (SELECT a.ArtistId, t.GenreId FROM Artist a INNER JOIN Album al ON a.ArtistId = al.ArtistId INNER JOIN Track t ON al.AlbumId = t.AlbumId GROUP BY(a.ArtistId, t.GenreId)) ArtistGenre INNER JOIN Artist a ON ArtistGenre.ArtistId = a.ArtistId INNER JOIN Genre g ON ArtistGenre.GenreId = g.GenreId GROUP BY (a.Name) ORDER BY COUNT(a.Name) DESC LIMIT 5"
-
->>>>>>> c7b4b96d73d6d12d50445043d19416477bb2e9b6
 
 const getMostCommonArtists = (request, response) => {
   db.query(GET_MOST_COMMON_ARTISTS, (error, results) => {
